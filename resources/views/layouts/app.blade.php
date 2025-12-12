60b5d7b63069d2332f1b8afe7416ffabf2fd2caa
efraim urel <efraimurelpalodang@gmail.com>
2025-12-12 17:03:36 +0800

    menambahkan @livewwire style dan script di layouts.app dan memperbaiki gambar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
     <link href="{{ asset('assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    @livewireStyles
 </head>
 
 <body id="page-top">
@@ -26,6 +27,7 @@
         </div>
     </div>
 
+    @livewireScripts
     <!-- JavaScript-->
     <script src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
